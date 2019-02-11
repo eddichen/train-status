5e0a7eafc04b27684625b32007c6b456205511c2
eddichen <contacteddichen@gmail.com>
2019-02-11 14:07:41 +0000

    updating the search result html structure

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,21 +4,26 @@ class SearchResults extends Component {
   render() {
     return (
       <table>
-        <tr>
-          <td>Due</td>
-          <td>Destination</td>
-          <td />
-          <td>Platform</td>
-        </tr>
-
-        {this.props.departureData.trains.trainServices.map((service, index) => (
-          <tr key={index}>
-            <td>{service.std}</td>
-            <td>{service.destination[0].locationName}</td>
-            <td>{service.etd}</td>
-            <td>{service.platform}</td>
+        <thead>
+          <tr>
+            <td>Due</td>
+            <td>Destination</td>
+            <td />
+            <td>Platform</td>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {this.props.departureData.trains.trainServices.map(
+            (service, index) => (
+              <tr key={index}>
+                <td>{service.std}</td>
+                <td>{service.destination[0].locationName}</td>
+                <td>{service.etd}</td>
+                <td>{service.platform}</td>
+              </tr>
+            )
+          )}
+        </tbody>
       </table>
     );
   }
